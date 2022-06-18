612c75551c4884a345f1c69cc5a97c2e34a0f0b5
Nikita Putilin <53861888+putnik-projects@users.noreply.github.com>
2022-06-18 12:57:19 +0300

    Configures prettier and pre-commit hook

--- .eslintrc.js
+++ .eslintrc.js
@@ -8,10 +8,8 @@ module.exports = {
     ecmaVersion: 12,
     sourceType: 'module',
   },
-  plugins: ['prettier'],
   rules: {
     'no-underscore-dangle': 0,
-    'prettier/prettier': 2,
     'import/no-extraneous-dependencies': [2, { devDependencies: true }],
   },
   root: true,
