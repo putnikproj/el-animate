4e78ee2d0166ba9abf1d00617813b21f7f63bc87
Nikita Putilin <53861888+putnik-projects@users.noreply.github.com>
2022-06-21 23:24:39 +0300

    Adds types to 'class-name'

--- src/helpers/class-name.js
+++
@@ -1,3 +0,0 @@
-export const has = (elem, className) => elem.classList.contains(className);
-export const add = (elem, className) => elem.classList.add(className);
-export const remove = (elem, className) => elem.classList.remove(className);
---
+++ src/helpers/class-name.ts
@@ -0,0 +1,3 @@
+export const has = (elem: HTMLElement, className: string) => elem.classList.contains(className);
+export const add = (elem: HTMLElement, className: string) => elem.classList.add(className);
+export const remove = (elem: HTMLElement, className: string) => elem.classList.remove(className);
