9d271d7826c5856f0bfaa6643b8435864993d7d9
Nikita Putilin <53861888+putnik-projects@users.noreply.github.com>
2022-07-09 07:41:36 +0000

    Configures transpilation code to es5

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,9 @@
 import { resolve } from 'path';
 import { defineConfig } from 'vite';
+import { babel } from '@rollup/plugin-babel';
 
-const LIB_NAME = 'ElAnimate';
+const LIB_NAME = 'el-animate';
+const LIB_NAME_CAMEL = 'ELAnimate';
 
 export default defineConfig({
   root: './playground',
@@ -10,9 +12,28 @@ export default defineConfig({
     emptyOutDir: true,
     sourcemap: true,
     lib: {
-      entry: resolve(__dirname, 'src/index.js'),
+      entry: resolve(__dirname, 'src/index.ts'),
       formats: ['cjs', 'es', 'iife', 'umd'],
-      name: LIB_NAME,
+      name: LIB_NAME_CAMEL,
+      fileName: (format) => {
+        if (format === 'cjs') {
+          return `${LIB_NAME}.js`;
+        }
+
+        return `${LIB_NAME}.${format}.js`;
+      },
+    },
+    rollupOptions: {
+      plugins: [
+        babel({
+          babelHelpers: 'bundled',
+          exclude: 'node_modules/**',
+          extensions: ['.js', '.ts'],
+        }),
+      ],
+      output: {
+        generatedCode: 'es5',
+      },
     },
   },
   server: { hmr: { clientPort: process.env.CODESPACES ? 443 : undefined } },
